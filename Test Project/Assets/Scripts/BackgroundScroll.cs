f59094fe8d21785ea60c5fd71e7964250d1e26c2
Sleepy122 <calder.jones3155@gmail.com>
2018-08-11 13:29:30 -0500

    Adding test project and testing first commit

---
+++ Test Project/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour {
+    Material material;
+    Vector2 offset;
+    public int xVelocity, yVelocity;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+        offset = new Vector2(xVelocity, yVelocity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        material.mainTextureOffset += offset * Time.deltaTime;
+	}
+}
