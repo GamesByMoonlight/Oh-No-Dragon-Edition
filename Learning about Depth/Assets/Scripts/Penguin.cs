2f0690130bf41fae01d2c0db1a57b00984011a1d
Sleepy122 <calder.jones3155@gmail.com>
2018-08-18 20:17:16 -0500

    added layering to simulate depth and player movment with arrow keys

---
+++ Learning about Depth/Assets/Scripts/Penguin.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Penguin : MonoBehaviour {
+    public float movementSpeed = 1.0F;
+    Rigidbody2D rb;
+    Tilemap tm;
+    GameObject temp;
+    Vector3Int penguinLocation;
+
+   // public GameObject penguin;
+
+	// Use this for initialization
+	void Start () {
+        rb = transform.GetComponent<Rigidbody2D>();
+        temp = GameObject.FindGameObjectWithTag("Tiles");
+        tm = temp.GetComponent<Tilemap>();
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+        penguinLocation.x = (int)rb.transform.position.x;
+        penguinLocation.y = (int)rb.transform.position.y;
+        penguinLocation.z = (int)rb.transform.position.z;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(Vector2.left * (movementSpeed * 2.5f));
+        }else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(Vector2.right * (movementSpeed * 2.5f));
+        }else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(Vector2.up * (movementSpeed * 2.5f));
+        }else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(Vector2.down * (movementSpeed * 2.5f));
+            
+            Debug.Log(tm.GetType().ToString());
+            Debug.Log(rb.transform.position.ToString());
+            Debug.Log(tm.layoutGrid.WorldToCell(rb.transform.position));
+            
+        }
+       
+
+        
+    }
+
+   
+}
