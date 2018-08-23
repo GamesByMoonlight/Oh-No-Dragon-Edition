9eeb5dfff0dddc09da839599bf61556f1dae87e7
Sleepy122 <calder.jones3155@gmail.com>
2018-08-23 00:35:40 -0500

    added my black void mechanic to auguys/september

--- Proto/Assets/Scripts/Penguin.cs
+++ Proto/Assets/Scripts/Penguin.cs
@@ -55,23 +55,36 @@ public class Penguin : MonoBehaviour {
             //-45,2,0 = darkness
             BoundsInt bounds = new BoundsInt(10, 10, 10, 10, 10, 10);
             Debug.Log(bounds.size);
+            Debug.Log(Resources.Load("bush_tile"));
            
         }
+
+        //Starting location of the black mass
         Vector3Int[] things = new Vector3Int[50];
+        // an array of tile base objects to be placed at the starting location of the black mass
         TileBase[] morethings = new TileBase[50];
+
+        // frame count is how long we are waiting until we increase the size of the black mass of tiles
         if (frameCount == encroachingDoomSpeed)
-        {
+        {   
+            //reset timer
             frameCount = 0;
             for (int x = 0; x < 50; x++)
             {
+                //(-20, 23, 0) is the base location of the black mass as i increment the for loop i am decrasing the y location which is going to drop down in a straight line
+                // The count is incremented each thime this loop is run increasing the x location by 1
                 things[x] = new Vector3Int(-20 + count, 23 - x, 0);
                 things[x] = new Vector3Int(-20 + count, 23 - x, 0);
+
+                // if any of the positions we are iterating through match the player location speed up the the rate of doom faster then the player can run ie kill em
                 if(things[x] == tm.layoutGrid.WorldToCell(rb.transform.position))
                 {
                     encroachingDoomSpeed = 1;
                 }
+                // because I cant generate tiles I placed a black tile out of the playable area this is grabbing that tile's TileBase to set it at the doom locaitons
                 morethings[x] = tm.GetTile(new Vector3Int(-45, -2, 0));
             }
+            //finally I set my array of new doom tiles to be black tiles and increment the count
             tm.SetTiles(things, morethings);
             count += 1;
         }
