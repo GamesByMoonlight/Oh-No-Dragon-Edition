3a7faca69f59c950d115c1731d25190412ae1d7a
Sleepy122 <Calder.Jones3155@gmail.com>
2018-08-24 00:01:03 -0500

    Testing unity and workspace (#34)
    * Fixen Merge Conflicts (#32)
    
    * Created Animation Folder and Dragon Sprites. Created scene "dragonAnimDemo" to demonstrate animation frames.
    Also saved white version of all dragon sprites.
    
    * Added white dragon sprite. Added animation to Player gameObject. Added animation controls and demonstration to player movement script (as new script, playermovementAnimated)
    
    * goodbye proto we will miss you RIP

--- August-September/Assets/Scripts/PlayerMovementAnimated.cs
+++ August-September/Assets/Scripts/PlayerMovementAnimated.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerMovementAnimated : MonoBehaviour {
 
@@ -12,6 +13,10 @@ public class PlayerMovementAnimated : MonoBehaviour {
     private float movePlayerHorizontal;
     private float movePlayerVertical;
     private Vector2 movement;
+    Tilemap tm;
+    public float frameCount = 0;
+    public int count = 0;
+    public int encroachingDoomSpeed = 10;
 
     //Added animator to script
     Animator anim;
@@ -28,6 +33,7 @@ public class PlayerMovementAnimated : MonoBehaviour {
         //defines animator
         anim = GetComponent<Animator>();
         playerRigidBody = GetComponent<Rigidbody2D>();
+        tm = GameObject.FindGameObjectWithTag("Tiles").GetComponent<Tilemap>();
     }
 
     // Update is called once per frame
@@ -42,9 +48,39 @@ public class PlayerMovementAnimated : MonoBehaviour {
 
         //returns value to animator. when animBaseSpeed is greater than 0.25 animation changes from idle to walking
         anim.SetFloat ("animBaseSpeed", Mathf.Abs (movePlayerHorizontal));
+        Debug.Log(tm.layoutGrid.WorldToCell(playerRigidBody.transform.position));
+        Debug.Log(tm.GetTile(tm.layoutGrid.WorldToCell(playerRigidBody.transform.position)));
+        // tm.SetTile(tm.layoutGrid.WorldToCell(playerRigidBody.transform.position), tm.GetTile(new Vector3Int(-2,-3,0)));
 
+        //Starting location of the black mass
+        Vector3Int[] locationOfDeath = new Vector3Int[60];
+        // an array of tile base objects to be placed at the starting location of the black mass
+        TileBase[] DeathTiles = new TileBase[60];
+        if (frameCount == encroachingDoomSpeed)
+        {
+            //reset timer
+            frameCount = 0;
+            for (int y = 0; y < DeathTiles.Length; y++)
+            {
+                //(-20, 23, 0) is the base location of the black mass as i increment the for loop i am decrasing the y location which is going to drop down in a straight line
+                // The count is incremented each thime this loop is run increasing the x location by 1
+                locationOfDeath[y] = new Vector3Int(-50 + count, 20 - y, 0);
+
+                // if any of the positions we are iterating through match the player location speed up the the rate of doom faster then the player can run ie kill em
+                if (locationOfDeath[y] == tm.layoutGrid.WorldToCell(playerRigidBody.transform.position))
+                {
+                    encroachingDoomSpeed = 1;
+                }
+                // because I cant generate tiles I placed a black tile out of the playable area this is grabbing that tile's TileBase to set it at the doom locaitons
+                DeathTiles[y] = tm.GetTile(new Vector3Int(-113, -10, 0));
+            }
+            //finally I set my array of new doom tiles to be black tiles and increment the count
+            tm.SetTiles(locationOfDeath, DeathTiles);
+            count += 1;
+        }
+        frameCount += 1;
         //calls animation method
-         SetAnimation();
+        SetAnimation();
     }
 
     //test animations for flying digging walking and breathing fire
