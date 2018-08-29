38917f9c2591cb351fd8013bfda396231126a173
Cisco <38115032+ciscom1971@users.noreply.github.com>
2018-08-28 22:20:33 -0500

    Cisco (#51)
    * timer I dont know the issue number, because we just worked this out. I dont want to use issue 42 but it is tied to it
    
    * Add DragonType Enum

---
+++ August-September/Assets/Scripts/DragonType.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonType : MonoBehaviour
+{
+    public enum eDragonType { AirDragon, WaterDragon, FireDragon, EarthDragon };
+
+
+    private eDragonType _DragonType;
+
+    public eDragonType DragonTypeV
+    {
+        get { return _DragonType; }
+        set { _DragonType = value; }
+    }
+
+    //DragonType.eDragonType d;
+
+    //d = DragonType.eDragonType.AirDragon;
+
+    //    switch (d)
+    //    {
+    //        case DragonType.eDragonType.AirDragon:
+    //        {
+    //                break;
+                   
+    //        }
+    //    }
+}
+
