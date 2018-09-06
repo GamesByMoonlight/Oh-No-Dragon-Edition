ae0e77c754a9985bbfa9fb2d8313421c2dc4208c
Lance Bailey <37199007+LanceBailey@users.noreply.github.com>
2018-09-06 16:43:47 -0500

    Feature/56 (#71)
    * Added red, blue, green, and yellow dragons, background, tiles for hole, tree, and rock
    
    * Added mob sprite and prefab. /25
    
    * Added water, mossy rock, spiked pits
    
    * Upated PlayerMovementAnimated to include names of the new tile types
    
    * Small code refactor, now when a tile is destroyed it is set to "null" rather than to a specific tile
    
    * Created an EventManager, and an EndZone prefab that triggers events on ending the level.
    
    Cleaned up the file system
    
    * Made the End Zone and Event Manager into prefabs
    
    * Triggering the 'End Zone' now loads the next scene after 3 seconds.  This number should be tweaked to give proper time for the animations, etc.
    
    Added extra scenes for testing, to be used to create levels.

--- August-September/Assets/Demo Assets/Demo Scripts/LevelLoader.cs
+++ August-September/Assets/Demo Assets/Demo Scripts/LevelLoader.cs
@@ -6,9 +6,46 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour {
 
-	public void LoadLevel(string levelToLoad)
+    void OnEnable()
+    {
+        EventManager.OnLevelEnd += LoadNextLevel;
+    }
+
+    void OnDisable()
+    {
+        EventManager.OnLevelEnd -= LoadNextLevel;
+    }
+
+    public void LoadLevel(string levelToLoad)
     {
         SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void LoadLevel(int levelIndexToLoad)
+    {
+        SceneManager.LoadScene(levelIndexToLoad);
+    }
+
+    void LoadNextLevel()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        StartCoroutine(DelayedLevelLoad(sceneIndex + 1));
+    }
+
+    IEnumerator DelayedLevelLoad(int nextSceneIndex)
+    {
+        yield return new WaitForSeconds(3f);
+
+        if (nextSceneIndex > SceneManager.sceneCountInBuildSettings - 1)
+        {
+            LoadLevel(0);
+        }
+        else
+        {
+            LoadLevel(nextSceneIndex);
+        }
+        
     }
 
 }
