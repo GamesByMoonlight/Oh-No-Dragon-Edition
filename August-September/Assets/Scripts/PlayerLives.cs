84cc45c55dc568168e32398fc3382c50ff05c5fb
cisco <ciscom1971@yahoo.com>
2018-09-19 22:54:50 -0500

    Player Lives feature 86.. i added it to mob movement.. messed up mob movement

---
+++ August-September/Assets/Scripts/PlayerLives.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLives : MonoBehaviour {
+
+    public int lives { get; set; }
+    public Text playerLives;
+
+    // Use this for initialization
+    void Start () {
+        lives = 3;
+        playerLives.color = Color.blue;
+        DontDestroyOnLoad(this.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        playerLives.text =  lives.ToString();
+    }
+
+    public void removeLife()
+    {
+        lives -= lives;
+        playerLives.text =  lives.ToString();
+
+    }
+
+
+
+}
