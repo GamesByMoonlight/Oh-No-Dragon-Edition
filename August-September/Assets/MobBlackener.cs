66b3dee48b782873d75d8ad718fc230daef8cb57
Lance <lance.bailey@gmail.com>
2018-09-26 18:17:47 -0500

    Mob now darkens as they burn

---
+++ August-September/Assets/MobBlackener.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobBlackener : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnEnable()
+    {
+        EventManager.OnLevelEnd += BlackenTheMob;
+    }
+
+    void OnDisable()
+    {
+        EventManager.OnLevelEnd -= BlackenTheMob;
+    }
+
+    void BlackenTheMob()
+    {
+        InvokeRepeating("SlowlyBurn", 1f, 0.1f);
+    }
+
+    void SlowlyBurn()
+    {
+        Color darkerColor = spriteRenderer.color;
+        darkerColor -= new Color(0.1f, 0.1f, 0.1f, 0);
+
+        spriteRenderer.color = darkerColor;
+
+        if (darkerColor == Color.black)
+        {
+            CancelInvoke("SlowlyBurn");
+        }
+    }
+}
